d0a82b93873ecb9b7d2a56bda3a70a25328ad8b9
Seba1119 <sebaalbasaleh@gmail.com>
2024-11-17 10:28:22 +0100

    deleted the route in seed folder en modified config next.js

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,20 +1,7 @@
-import { NextConfig } from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true, // Enables React Strict Mode for better error handling
-  images: {
-    domains: ['example.com'], // Add external image domains you want to allow
-  },
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL, // Environment variables (replace with actual variable)
-  },
-  // Typing config as 'any' to avoid errors
-  webpack(config: any, { isServer }: { isServer: boolean }) {
-    if (!isServer) {
-      config.resolve.fallback = { fs: false }; // Handle conflicts for client-side with Node.js built-in modules
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
